6bc0e640320215b9ed53e3b456778591ce33c962
juliano batistim <jubatistim@gmail.com>
2022-05-27 11:39:01 -0500

    adjusted visibility of constructor

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.9;
 contract Inbox {
     string public message;
 
-    constructor (string memory initialMessage) public {
+    constructor (string memory initialMessage) {
         message = initialMessage;
     }
 
